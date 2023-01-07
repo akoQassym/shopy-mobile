7c0ed796377bc1104cce0f691cea2e337b75c4d9
Aknur <kassym.aknur@gmail.com>
2023-01-07 22:50:35 +0600

    feat: updated App.js - navigation starter, font setup, etc

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
+import { useCallback, useContext } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ActivityIndicator,
+  View,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+
+import AuthContextProvider from './store/authContext';
+import { AuthContext } from './store/authContext';
+
+import AuthenticationStack from './navigation/AuthenticationStack';
+import BottomTabsNavigation from './navigation/BottomTabsNavigation';
+
+const Navigation = () => {
+  const authCtx = useContext(AuthContext);
+  console.log(authCtx.isAuthenticated);
+
+  return (
+    <NavigationContainer>
+      {!authCtx.isAuthenticated && <AuthenticationStack />}
+      {authCtx.isAuthenticated && <BottomTabsNavigation />}
+    </NavigationContainer>
+  );
+};
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-regular': require('./assets/fonts/Roboto/Rubik-Regular.ttf'),
+    'Roboto-bold': require('./assets/fonts/Roboto/Rubik-Bold.ttf'),
+    'Roboto-bold-italic': require('./assets/fonts/Roboto/Rubik-BoldItalic.ttf'),
+    'Roboto-semi-bold': require('./assets/fonts/Roboto/Rubik-SemiBold.ttf'),
+    'Roboto-semi-bold-italic': require('./assets/fonts/Roboto/Rubik-SemiBoldItalic.ttf'),
+    'Roboto-extra-bold': require('./assets/fonts/Roboto/Rubik-ExtraBold.ttf'),
+    'Roboto-extra-bold-italic': require('./assets/fonts/Roboto/Rubik-ExtraBoldItalic.ttf'),
+    'Roboto-italic': require('./assets/fonts/Roboto/Rubik-Italic.ttf'),
+    'Roboto-medium': require('./assets/fonts/Roboto/Rubik-Medium.ttf'),
+    'Roboto-medium-italic': require('./assets/fonts/Roboto/Rubik-MediumItalic.ttf'),
+    'Roboto-light': require('./assets/fonts/Roboto/Rubik-Light.ttf'),
+    'Roboto-light-italic': require('./assets/fonts/Roboto/Rubik-LightItalic.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <SafeAreaView style={styles.rootScreen} onLayout={onLayoutRootView}>
+        <AuthContextProvider>
+          <Navigation />
+        </AuthContextProvider>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontFamily: 'Roboto-regular',
   },
 });
