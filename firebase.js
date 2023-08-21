0f217e3b218246929494dec6dd0e6d0cf8373a1c
Aknur <kassym.aknur@gmail.com>
2023-08-22 01:54:01 +0600

    refactor: remove invalid firebase API keys

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAdmzFk12bvVUWZQchJnchjuSsnADSn4bc',
-  authDomain: 'shopy-25a24.firebaseapp.com',
-  projectId: 'shopy-25a24',
-  storageBucket: 'shopy-25a24.appspot.com',
-  messagingSenderId: '334805858613',
-  appId: '1:334805858613:web:d01745e6dabc349f54347c',
-  measurementId: 'G-XVNKKC2CX4',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
